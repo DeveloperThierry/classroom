b7b551c19e9842e2ec3461d38600d4cdb0f36e09
developerthierry <developerthierry@gmail.com>
2026-05-14 04:12:48 +0000

    implement routes

--- classroom-backend/src/index.ts
+++ classroom-backend/src/index.ts
@@ -1,8 +1,18 @@
 import express from 'express'
+import cors from 'cors'
+import subjectsRouter from './routes/subjects'
 
 const app = express()
 const PORT = 8000
 app.use(express.json())
+app.use(cors({
+    // origin:[process.env.FRONTEND_URL!, process.env.FIREBASE_URL!],
+    origin:process.env.FRONTEND_URL,
+    methods:['GET', 'PUT', 'POST', 'DELETE'],
+    credentials:true
+}))
+app.use('/api/subjects', subjectsRouter)
+
 app.get('/', (req, res) => {
     res.send('Hello, welcome to the Classroom API!')
 })
