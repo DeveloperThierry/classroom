370aea9ed1f052d201bdb857be3c8d4b3f825aab
developerthierry <developerthierry@gmail.com>
2026-05-14 04:49:45 +0000

    implement suggestions from coderabbit

--- classroom-backend/src/index.ts
+++ classroom-backend/src/index.ts
@@ -4,13 +4,15 @@ import subjectsRouter from './routes/subjects'
 
 const app = express()
 const PORT = 8000
-app.use(express.json())
+
+if(!process.env.FRONTEND_URL) throw new Error('FRONTEND_URL is not set in .env file.')
+
 app.use(cors({
-    // origin:[process.env.FRONTEND_URL!, process.env.FIREBASE_URL!],
     origin:process.env.FRONTEND_URL,
     methods:['GET', 'PUT', 'POST', 'DELETE'],
     credentials:true
 }))
+app.use(express.json())
 app.use('/api/subjects', subjectsRouter)
 
 app.get('/', (req, res) => {
